b438732d20fef2ae864b1273d2511e4847ebdc8f
Keyology <keonimurray45@gmail.com>
2019-04-04 11:09:40 -0700

    refactored code

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -3,18 +3,23 @@ import random
 import sys
 
 
-def random_sentence():
+def get_words(file_name):
+    words = [line.strip() for line in open(file_name)]
+    return words
 
-    word = [line.strip() for line in open("/usr/share/dict/words")]
-    user_input = sys.argv[1:]
-    new_sentence = []
-    counter = 0
-    while counter < 5:
-        new_sentence.append(word[random.randint(0, len(word) - 1)])
-        counter += 1
-    final_word = " ".join(new_sentence)
-    print(final_word)
+
+def random_sentence(words_list, num_words):
+
+    new_words = []
+    for idx in range(0, num_words):
+        random_index = random.randint(0, len(words_list) - 1)
+        new_words.append(words_list[random_index])
+    final_sentence = " ".join(new_words)
+    return final_sentence
 
 
 if __name__ == '__main__':
-    random_sentence()
+    user_input = sys.argv[1]
+    words = get_words("/usr/share/dict/words")
+    for i in range(0,  int(sys.argv[2])):
+        print(random_sentence(words, int(user_input)))
