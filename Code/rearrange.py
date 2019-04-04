812326ff244f62b889ded6d4dfb8b37a5f51db8c
Keyology <keonimurray45@gmail.com>
2019-04-03 18:18:27 -0700

    error handling

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -7,19 +7,23 @@ user_input = sys.argv[1:]
 
 def random_words(words):
     # loop over array length
-    for i in range(len(words) - 1, 0, -1):
-        # generate a random number
-        random_index = random.randint(0, i+1)
-        # swap the words based on index
-        words[i], words[random_index] = words[random_index], words[i]
-        # join the words to form a sentence
-    sentence = " ".join(words)
-    # print it to the user
+    try:
+        for i in range(len(words) - 1, 0, -1):
+            # generate a random number
+            random_index = random.randint(0, i+1)
+            # swap the words based on index
+            words[i], words[random_index] = words[random_index], words[i]
+            # join the words to form a sentence
+        sentence = " ".join(words)
+        # print it to the user
 
-    user_input.clear()
-    sys.argv = [sys.argv[0]]
-    return sys.stdout.write(str(sentence) + '\n')
+        user_input.clear()
+        sys.argv = [sys.argv[0]]
+        return sys.stdout.write(str(sentence) + '\n')
+    except:
+        print("Could not shuffle words")
 
 
 if __name__ == '__main__':
+
     random_words(user_input)
