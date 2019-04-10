f1b0d8d7ec89b76c4524e3783ac03dfcc33d7f02
Keyology <keonimurray45@gmail.com>
2019-04-09 17:48:53 -0700

    completed hsitogram challenge

--- Code/histogram.py
+++ Code/histogram.py
@@ -18,75 +18,88 @@ def get_words(file_name):
 
 
 def get_words_counts_dict(words_list):
+    # create an empty dict
     word_counts = {}
 
+    # loop over words in list
     for words in words_list:
+        # check if word is in word counts dict
         if words in word_counts:
+            # if it is increase the value by 1
             word_counts[words] += 1
         else:
+            # otherwise set the value to 1
             word_counts[words] = 1
     return word_counts
 
 
-# def get_words_counts_list(word_list):
-
-#     new_word_list = word_list.split(" ")
-#     word_count = []
-
-#     for word in new_word_list:
-#         for index in word_count:
-#             if index[0] == word:
-#                 index[1] += 1
-#         word_count.append([word, 1])
-#     return word_count
-
-
 def get_words_counts_list(words_list):
+    # sort the list
     words_list.sort()
+    # create empty list
     new_words_list = []
-
+    # loop over words list
     for word in words_list:
-
+        # check if nothing is in the list
         if len(new_words_list) is 0:
+            # if nothing is in the list append to the list
             new_words_list.append([word, 1])
         else:
+            # otherwise check if word is the same as the last value added to the list
             if word == new_words_list[-1][0]:
+                # increase the last value by 1
                 new_words_list[-1][1] += 1
             else:
+                # otherwise append to the list
                 new_words_list.append([word, 1])
 
     return new_words_list
 
-    # new_words_list.remove(words)
-    # new_words_list.append([words, 1])
-    # for i in range(0, len(new_words_list)):
-    #     if new_words_list[i][0] == words:
-    #         new_words_list[i][1] += 1
-    #         del new_words_list[-1]
 
-    # for words in words_list:
-    #     word_counts.append([words])
-    #     if words in word_counts:
-    #         print("***HAS BEEN HIT***!")
-    #         word_counts[words][1] += 1
+def get_words_counts_tuple(text):
+    # create empty list
+    words = []
+    # get words froms list
+    for word in text:
+        # check if word is found
+        found = False
+        # looping through list
+        for index in words:
+            # check if word is in list
+            if index[0] == word:
+                #  increase list value
+                freq = index[1] + 1
+                # remove duplicates
+                words.remove(index)
+                # append word and value
+                words.append((word, freq))
+                # check if word is found
+                found = True
+        if not found:
+            # if word is not found append list
+            words.append((word, 1))
 
-    #         print("****WORDS****", word_counts[words][1])
-    # return word_counts
+    return words
 
 
-#         # else:
-#         word_counts[words][1] += 1
-#         print("WORDS LIST COUNT", word_counts[0])
-#     return word_counts
-    # sort words_list
-    # create empty array
-    # check if word is in list
-    # if it is set word to none
-    # then update last item value += 1
-    # otherwise update array
-    # then return it
+def unique_words(histogram):
+    # returns number of unique words in histogram
+    total_count = sum(histogram.values())
+    return total_count
+
+
+def frequency(word, histogram):
+    # checks if word is in histogram
+    if word in histogram:
+        # returns key value pairs
+        return histogram[word]
+    else:
+        return "That word is not in the histogram"
+
+
 if __name__ == "__main__":
 
     words_list = get_words('animals.txt')
     words_counts = get_words_counts_list(words_list)
-    print("WORDS COUNT", words_counts)
+    new_tuple = get_words_counts_dict(words_counts)
+    print("WORDS COUNT", new_tuple)
