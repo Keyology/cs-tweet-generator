a0ebf5b9f43a45a10f79a6bfe051b8f2b7cb7e6b
Keyology <keonimurray45@gmail.com>
2019-04-22 20:08:32 -0700

    updated stochastic sampler

--- Code/stochastics.py
+++ Code/stochastics.py
@@ -1,17 +1,42 @@
 from histogram import get_words
+from histogram import get_words_counts_dict
 import random
 
 
-def test_probability(histogram):
-    # created list to append results
-    results = []
-    # number of times I want to run the test
-    number_of_times = 1000
+# def test_sampler(histogram, number):
+#     # created list to append results
+#     result = []
+#     # the number of times I want to run the test
 
-    # add sample word to list  1000 times
-    for i in range(0, number_of_times):
-        results.append(sampler(histogram))
-    return results
+#     # appending sample word to list n times
+#     for i in range(0, number):
+#         result.append(sampler(histogram))
+#     return result
+
+
+# def test_sampler(histogram, number):
+
+#             number_wins[key] += 1
+#         else:
+#             number_wins[key] = 1
+#     print("NUMBER OF WINS", number_wins)
+
+
+def test_sampler(words, number):
+    number_wins = {}
+
+    #  for key, value in dictionary.items():
+    #     pairs = f'{key} = {value/length}'
+    #     print(pairs)
+    #     break
+
+    for i in range(0, number):
+        result = sampler(words)
+        if result in number_wins:
+            number_wins[result] += 1
+        else:
+            number_wins[result] = 1
+    return number_wins
 
 
 def sampler(words):
@@ -22,12 +47,25 @@ def sampler(words):
     for word in words:
         dictionary[word] = words.count(word)
 
+    random_value = random.random()
+    total_value = sum(dictionary.values())
+    total = 0
+    # number_wins = {}
+
     for key, value in dictionary.items():
-        pairs = f'{key} = {value/length}'
-        print(pairs)
+        total += value
+        if total/total_value >= random_value:
+            return key
+            # if key in number_wins:
+            #     number_wins[key] += 1
+            # else:
+            #     number_wins[key] = 1
+            # break
 
 
 if __name__ == '__main__':
     words = get_words('animals.txt')
-    sampler(words)
-    test_probability(words)
+    histogram_dict = get_words_counts_dict(words)
+
+    # sampler(words)
+    print(test_sampler(words, 10000))
