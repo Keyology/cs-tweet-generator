bc05c4dddf085fd6d7100022b66956f388135a74
Keyology <keonimurray45@gmail.com>
2019-05-14 10:47:59 -0700

    refactored markov chain

--- Code/markov.py
+++ Code/markov.py
@@ -23,53 +23,63 @@ def first_order(corpus_of_words):
 
 
 def second_order(corpus_of_words):
-    words = corpus_of_words
-    chain = {}
+    words = corpus_of_words  # list of strings
+    chain = {}  # dict to hold Markov states, key: word, value: histogram
     corpus_length = len(words)
 
-    for i, key1 in enumerate(words):
-        if corpus_length > (i + 2):
-            key2 = words[i + 1]
-            word = words[i + 2]
-            if(key1, key2) not in chain:
-                chain[(key1, key2)] = Dictogram([word])
+    for i, word1 in enumerate(words):
+        if i + 2 >= corpus_length:
+            break  # skip last two iterations
 
-            else:
-                chain[(key1, key2)].add_count(word)
+        word2 = words[i + 1]
+        word3 = words[i + 2]
+        if (word1, word2) not in chain:
+            chain[(word1, word2)] = Dictogram()
+
+        chain[(word1, word2)].add_count(word3)
 
     return chain
 
 
+def sentencize(word_list):
+    word_list[0] = word_list[0].capitalize()
+    word_list.append(".")
+    return " ".join(word_list)
+
+
 def create_sentence(markov_chain):
-    sentence = list()
+    word_list = list()
 
     # start_word = list(markov_chain.keys())[0]
     start_word = random.choice(list(markov_chain.keys()))
-
-    sentence.append(start_word)
+    word_list.append(start_word)
 
     for _ in range(0, 10):
+        last_word = word_list[-1]
+        histogram = markov_chain.get(last_word)
+        random_word = sampler(histogram)
+        word_list.append(random_word)
 
-        get_value = markov_chain.get(sentence[-1])
-        random_word = sampler(get_value)
-        sentence.append(random_word)
-    sentence[-1] = "."
-    return " ".join(sentence)
+    return sentencize(word_list)
 
 
 def second_order_sentence(markov_chain):
-    start_words = random.choice(list(markov_chain.keys()))
-    sentence = list(start_words)
-    track_words = start_words
+    word_list = list()
+    start_word_tuple = random.choice(list(markov_chain.keys()))
+    for start_word in start_word_tuple:
+        word_list.append(start_word)
+
+    track_words = start_word_tuple
 
     for _ in range(0, 12):
-        get_value = markov_chain.get(track_words)
-        random_word = sampler(get_value)
-        sentence.append(random_word)
+        histogram = markov_chain.get(track_words)
+        random_word = sampler(histogram)
+        word_list.append(random_word)
+        # updating tuple of last words that I am tracking
         track_words = (track_words[-1], random_word)
-    sentence[0] = sentence[0].capitalize()
-    sentence[-1] = "."
-    return " ".join(sentence)
+
+    # Capitalize first word, add period and spaces
+    return sentencize(word_list)
 
 
 if __name__ == "__main__":
